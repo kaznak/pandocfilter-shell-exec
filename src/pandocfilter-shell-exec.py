fd1ec071204ed197618d08f4ddd6b832a8070a2e
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2018-03-29 12:05:09 +0900

    feat(001.simple): add simple execution feature

---
+++ src/pandocfilter-shell-exec.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import panflute as pf
+from subprocess import Popen, PIPE
+
+#############################################################
+def proc_shell_exec(elm, doc):
+    prog = u'sh'
+    code = elm.text
+
+    sys.stderr.write(
+        'shell-exec #' + elm.identifier + ' prog=' + prog + '\n'
+    )
+    
+    p = Popen([prog], stdin=PIPE, stdout=PIPE)
+    p.stdin.write(code.encode('utf-8'))
+    ret = p.communicate()[0].decode('utf-8')
+    p.stdin.close
+
+    return ret
+
+#############################################################
+def pandoc_filter(elm, doc):
+    # print((elm,doc), file=sys.stderr)
+    if type(elm) == pf.CodeBlock and 'shell-exec' in elm.classes:
+        elm.text = proc_shell_exec(elm, doc)
+        return elm
+
+#############################################################
+if __name__ == "__main__":
+    pf.run_filters(
+        [
+            pandoc_filter,
+        ],
+        doc = None)
