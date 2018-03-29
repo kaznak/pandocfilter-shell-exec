0b4e84c59d9774d7fdde2f721d7e6cb7a64e9312
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2018-03-29 16:07:47 +0900

    feat(002.image): image element

--- src/pandocfilter-shell-exec.py
+++ src/pandocfilter-shell-exec.py
@@ -5,6 +5,7 @@ import sys
 import json
 import panflute as pf
 from subprocess import Popen, PIPE
+import collections as c
 
 #############################################################
 def proc_shell_exec(elm, doc):
@@ -22,12 +23,41 @@ def proc_shell_exec(elm, doc):
 
     return elm
 
+def proc_image(elm,doc):
+    attr = elm.attributes
+
+    altstr = attr.get('alt','an image')
+    if 'alt' in attr:
+        del attr['alt']
+    title = attr.get('title','an image')
+    if 'title' in attr:
+        del attr['title']
+
+    return pf.Para(pf.Image(
+        pf.Str(altstr),
+        url = elm.text.strip(),
+        title = title,
+        identifier = elm.identifier,
+        classes = elm.classes,
+        attributes = attr
+    ))
+
 #############################################################
 def pandoc_filter(elm, doc):
     # print((elm,doc), file=sys.stderr)
     if type(elm) == pf.CodeBlock and 'shell-exec' in elm.classes:
         elm = proc_shell_exec(elm, doc)
-        return elm
+
+        if 'image' in elm.classes:
+            sys.stderr.write(
+                'shell-exec #' + elm.identifier + ' output as Image\n'
+            )
+            return proc_image(elm, doc)
+        else:
+            sys.stderr.write(
+                'shell-exec #' + elm.identifier + ' output as CodeBlock \n'
+            )
+            return elm
 
 #############################################################
 if __name__ == "__main__":
